035ec569ce0f48268a86c4c089a132bb66da63a8
Eric Still <58347689+Eric02851@users.noreply.github.com>
2021-07-16 16:19:26 -0700

    Update main.py
    Added instructions text to startup
    Fixed phantom bug

--- main.py
+++ main.py
@@ -4,23 +4,49 @@ import pyautogui
 from global_hotkeys import *
 import time
 
+pyautogui.FAILSAFE = False
+pyautogui.PAUSE = 1E-20
+
+instructions = """
+
+ /$$    /$$          /$$                                          /$$           /$$                                 /$$      /$$                     /$$       /$$                    
+| $$   | $$         | $$                                         | $$          | $$                                | $$$    /$$$                    | $$      |__/                    
+| $$   | $$ /$$$$$$ | $$  /$$$$$$   /$$$$$$  /$$$$$$  /$$$$$$$  /$$$$$$        | $$        /$$$$$$   /$$$$$$       | $$$$  /$$$$  /$$$$$$   /$$$$$$$| $$$$$$$  /$$ /$$$$$$$   /$$$$$$ 
+|  $$ / $$/|____  $$| $$ /$$__  $$ /$$__  $$|____  $$| $$__  $$|_  $$_/        | $$       |____  $$ /$$__  $$      | $$ $$/$$ $$ |____  $$ /$$_____/| $$__  $$| $$| $$__  $$ /$$__  $$
+ \  $$ $$/  /$$$$$$$| $$| $$  \ $$| $$  \__/ /$$$$$$$| $$  \ $$  | $$          | $$        /$$$$$$$| $$  \ $$      | $$  $$$| $$  /$$$$$$$| $$      | $$  \ $$| $$| $$  \ $$| $$$$$$$$
+  \  $$$/  /$$__  $$| $$| $$  | $$| $$      /$$__  $$| $$  | $$  | $$ /$$      | $$       /$$__  $$| $$  | $$      | $$\  $ | $$ /$$__  $$| $$      | $$  | $$| $$| $$  | $$| $$_____/
+   \  $/  |  $$$$$$$| $$|  $$$$$$/| $$     |  $$$$$$$| $$  | $$  |  $$$$/      | $$$$$$$$|  $$$$$$$|  $$$$$$$      | $$ \/  | $$|  $$$$$$$|  $$$$$$$| $$  | $$| $$| $$  | $$|  $$$$$$$
+    \_/    \_______/|__/ \______/ |__/      \_______/|__/  |__/   \___/        |________/ \_______/ \____  $$      |__/     |__/ \_______/ \_______/|__/  |__/|__/|__/  |__/ \_______/
+                                                                                                    /$$  \ $$                                                                         
+                                                                                                   |  $$$$$$/                                                                         
+                                                                                                    \______/                                                                          
+
+Usage and Keybinds:
+1. alt + shift + 7
+    Opens buy menue, then buys and drops shorties.
+
+2. alt + shift + 8
+    Spam clicks f to rapidly pick up shorties lagging the server.
+
+3. alt + shift + 8
+    Stops running action.
+"""
+
 resolution = pyautogui.size()
+print(instructions)
 print(resolution)
 
 is_alive = True
 
 action = None
 
-pyautogui.FAILSAFE = False
-pyautogui.PAUSE = 1E-20
-
 def loop():
     global action
     while action == True:
+        time.sleep(0.1)
         pyautogui.click()
         time.sleep(0.1)
         pyautogui.press('g')
-        time.sleep(0.1)
     while action == False:
         pyautogui.press('f')
 
@@ -43,11 +69,6 @@ def stop():
     global action
     action = None
 
-def exit_application():
-    global is_alive
-    stop_checking_hotkeys()
-    is_alive = False
-
 bindings = [
     [["alt", "shift", "7"], None, buy],
     [["alt", "shift", "8"], None, pickup],
@@ -58,5 +79,3 @@ register_hotkeys(bindings)
 start_checking_hotkeys()
 while is_alive:
     loop()
-    #time.sleep(0.001)
-
